0078907d7c3bfbab6086dca3b6648c55c2e47cef
Dima Dyachenko <Dyachenko722@gmail.com>
2015-11-20 14:56:48 +0200

    Copy,cut,fixed,drive d

--- Site/Site/Controllers/FilesController.cs
+++ Site/Site/Controllers/FilesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Text;
 using System.Management.Automation;
+using System.IO;
 
 namespace Site.Controllers
 {
@@ -21,7 +22,7 @@ namespace Site.Controllers
         //
         public ActionResult ListOfFile(string str)
         {
-            
+
             var shell = PowerShell.Create();
             shell.Commands.AddScript(" Get-ChildItem '" + str + "' |  Select-Object Name, Mode, LastWriteTime, FullName  | ConvertTo-Json");
             var results = shell.Invoke();
@@ -29,19 +30,19 @@ namespace Site.Controllers
             {
 
                 var builder = new StringBuilder();
-                
+
 
 
                 foreach (var psObject in results)
                 {
                     builder.Append(psObject.ToString() + "\r\n");
-                   
+
                 }
                 string Result = builder.ToString();
                 return Content(Result);
             }
 
-            return Content("Папка пуста!","text/plain");
+            return Content("Папка пуста!", "text/plain");
         }
 
         //[HttpGet]
@@ -49,26 +50,75 @@ namespace Site.Controllers
         {
 
             string s = type.ToString();
-            
-            
-            if(type=="2")
+
+
+            if (type == "2")
             {
                 var shell = PowerShell.Create();
-                    shell.Commands.AddScript(" Copy-Item  " + file + "  " + path);
-                    var results = shell.Invoke();
+                shell.Commands.AddScript(" Copy-Item  " + file + "  " + path);
+                var results = shell.Invoke();
             }
             if (type == "1")
             {
                 var shell = PowerShell.Create();
-                    shell.Commands.AddScript(" Move-Item  '" + file + "'  '" + path + "'");
-                    var results = shell.Invoke();
+                shell.Commands.AddScript(" Move-Item  '" + file + "'  '" + path + "'");
+                var results = shell.Invoke();
             }
 
-            string res = "Done";
-            return Content(res);
+
+            return Content(path);
+
         }
 
-        
-        
-	}
+        public ActionResult OpenFile(string str)
+        {
+
+            StreamReader sr = new StreamReader(str);
+            string strin = sr.ReadToEnd().ToString();
+            sr.Close();
+
+
+            return Content(strin, "text/plain");
+
+        }
+        public ActionResult RewriteToFile(string path, string str)
+        {
+            string s = path;
+            string s1 = str;
+            StreamWriter sw = new StreamWriter(path);
+            sw.WriteLine(str);
+
+
+            sw.Close();
+
+
+            return Content("done", "text/plain");
+
+        }
+        public ActionResult CreateFile(string str)
+        {
+            string ss = str;
+            var shell = PowerShell.Create();
+            shell.Commands.AddScript(" New-Item " + str + " -type file");
+            var results = shell.Invoke();
+            return Content("Done", "text/pain");
+        }
+        public ActionResult CreateDirectory(string str)
+        {
+            string ss = str;
+            var shell = PowerShell.Create();
+            shell.Commands.AddScript(" New-Item " + str + " -type directory");
+            var results = shell.Invoke();
+            return Content("Done", "text/pain");
+        }
+        public ActionResult Delete(string str)
+        {
+            string ff = str;
+            var shell = PowerShell.Create();
+            shell.Commands.AddScript(" Remove-Item  '" + str + "' -recurse");
+            var results = shell.Invoke();
+            return Content("Done", "text/pain");
+        }
+
+    }
 }
